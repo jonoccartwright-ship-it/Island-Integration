b2d176ef3685beaba16ddde074d1eaf08cc17796
jonoccartwright-ship-it <jonoccartwright@gmail.com>
2025-08-25 22:22:25 -0400

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,3 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-// IMPORTANT: change base to your repo name, e.g. "/island-site/"
-export default defineConfig({
-  plugins: [react()],
-  base: "/YOUR_REPO_NAME/"
-});
+export default defineConfig({ plugins:[react()], base:"/YOUR_REPO_NAME/" });
