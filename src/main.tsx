15b0fa5b1867c96415ffc25a9e00667b36b40b36
jonoccartwright-ship-it <jonoccartwright@gmail.com>
2025-08-25 22:42:03 -0400

    Delete src directory

--- src/main.tsx
+++
@@ -1,4 +0,0 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import IslandIntegrationSite from "./App";
-createRoot(document.getElementById("root")!).render(<IslandIntegrationSite />);
